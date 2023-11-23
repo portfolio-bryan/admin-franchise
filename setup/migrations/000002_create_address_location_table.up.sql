d011f8665a16719a1eccdb92fd00cf4a346c0ce0
bperezgo <bsperezgo@gmail.com>
2023-11-22 21:17:58 -0500

    fix: constraint of address location migration

--- setup/migrations/000002_create_address_location_table.up.sql
+++ setup/migrations/000002_create_address_location_table.up.sql
@@ -2,8 +2,9 @@ CREATE TABLE IF NOT EXISTS address_location (
   id uuid,
   `address` VARCHAR(64) NOT NULL,
   zip_code VARCHAR(64) NOT NULL,
+  location_id uuid NOT NULL,
   PRIMARY KEY (id)
-  CONSTRAINT fk_user
-    FOREIGN KEY(user_id) 
-	    REFERENCES users(id)
+  CONSTRAINT fk_location
+    FOREIGN KEY(location_id) 
+	    REFERENCES locations(id)
 );
